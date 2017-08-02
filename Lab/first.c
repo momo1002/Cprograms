5e586e599d74678ae8caff5ef998227aa3b4b24e
momo1002 <momoko.nakada1002@gmail.com>
2017-08-02 16:26:57 -0700

    add ex23 to convert hex to dec

--- Lab/first.c
+++ Lab/first.c
@@ -8,4 +8,21 @@ int main(){
         scanf("%d %d", &num1, &num2);
 	int sum = num1 + num2;
 	printf("%d plus %d equals %d.\n",num1, num2, sum);
+
+	int num3;
+        int num4;
+		
+        int arr1[5];
+        int arr2[5];
+
+	for(int i = 0;i < 5; i++){
+		printf("Please enter integers with a space: ");
+		scanf("%d %d", &num3, &num4);
+
+		arr1[i] = (int) num3;
+		arr2[i] = (int) num4;	
+	}
+	for(int i = 0;i < 5 ;i++){
+		printf("%d %d\n",arr1[i], arr2[i]);
+	}
 }
---
+++ Lab/squeeze.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+	char start[5];
+	char end[5];
+	stract(start,end);
+}
+
+void stract(char s[], char t[])
+        {
+                int i, j;
+
+                i = j = 0;
+                while (s[i] != '\0')
+                        i++;
+                while ((s[i++] = t[j++]) != '0')
+                        ;
+        }
