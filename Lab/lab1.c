4509b7172e90a4496531d31439a967f79a275422
momo1002 <momoko.nakada1002@gmail.com>
2017-08-02 13:03:59 -0700

    fixed 'int' main()

--- Lab/lab1.c
+++ Lab/lab1.c
@@ -1,36 +1,37 @@
 #include <stdio.h>
 
-main(){
-	printf("Converting Decimal to Binary 12 = 1100¥n");
-	printf("Converting Decimal to Binary 185 = 10111001¥n");
-	printf("Converting Decimal to Binary 294 = 100100110¥n");
 
-	printf("Converting Binary to Decimal 101101 = 47¥n");
-	printf("Converting Binary to Decimal 11101011 = 253¥n");
-        printf("Converting Binary to Decimal 1010 = 10¥n");
+int main(){
+	printf("Converting Decimal to Binary 12 = 1100\n");
+	printf("Converting Decimal to Binary 185 = 10111001\n");
+	printf("Converting Decimal to Binary 294 = 100100110\n");
 
-        printf("Converting Decimal to Hexadecimal 0x138 = 8A¥n");
-        printf("Converting Decimal to Hexadecimal 0x119 = 77¥n);
-        printf("Converting Decimal to Hexadecimal 0x59 = 3B¥n");
+	printf("Converting Binary to Decimal 101101 = 47\n");
+	printf("Converting Binary to Decimal 11101011 = 253\n");
+        printf("Converting Binary to Decimal 1010 = 10\n");
 
-        printf("Converting Hexadecimal to Decimal 0x1A= 26¥n");
-        printf("Converting Hexadecimal to Decimal 0x3F31 = 16177¥n");
-        printf("Converting Hexadecimal to Decimal 0x31C = 796¥n");
+        printf("Converting Decimal to Hexadecimal 0x138 = 8A\n");
+        printf("Converting Decimal to Hexadecimal 0x119 = 77\n");
+        printf("Converting Decimal to Hexadecimal 0x59 = 3B\n");
 
-        printf("Converting Binary to Hexadecimal 10010111 = 97¥n");
-        printf("Converting Binary to Hexadecimal 101101111110 = B7E¥n");
-        printf("Converting Binary to Hexadecimal 1111 = F¥n");
+        printf("Converting Hexadecimal to Decimal 0x1A= 26\n");
+        printf("Converting Hexadecimal to Decimal 0x3F31 = 16177\n");
+        printf("Converting Hexadecimal to Decimal 0x31C = 796\n");
 
-        printf("Converting Hexadecimal to Binary 1A3B = 0001101000111011¥n");
-        printf("Converting Hexadecimal to Binary ABCD = 1010101111001101¥n");
-        printf("Converting Hexadecimal to Binary 1111 = 0001000100010001¥n");
+        printf("Converting Binary to Hexadecimal 10010111 = 97\n");
+        printf("Converting Binary to Hexadecimal 101101111110 = B7E\n");
+        printf("Converting Binary to Hexadecimal 1111 = F\n");
+
+        printf("Converting Hexadecimal to Binary 1A3B = 0001101000111011\n");
+        printf("Converting Hexadecimal to Binary ABCD = 1010101111001101\n");
+        printf("Converting Hexadecimal to Binary 1111 = 0001000100010001\n");
 
 
-        printf("----------------------------------------------------------¥n");
+        printf("----------------------------------------------------------\n");
 
 	int num1 = 2;
 	int num2 = 3;
 	int sum = num1+num2;
 
-        printf("%d plus %d equals %d¥n", num1,num2,sum);
+        printf("%d plus %d equals %d\n", num1,num2,sum);
 }
