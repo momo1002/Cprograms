0ab82c06e7bef5314c7a82bd424a44d05fde7f7a
momo1002 <momoko.nakada1002@gmail.com>
2017-08-06 13:33:16 -0700

    add asm2-1,2

---
+++ Assignment/asm2-2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int i;
+
+    printf("statement 1\n");
+    printf("statement 2\n");
+    for(i = 0; i < 10; i++)
+        printf("statement 3\n");
+        printf("statement 4\n");
+    
+    printf("statement 5\n");
+
+    return 0;
+}
---
+++ Assignment/asm2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int i;
+    for(i = 0; i < 3; i = i + 1)
+        printf("a\n");
+        printf("b\n");
+
+    printf("c\n");
+}
